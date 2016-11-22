6729b12d10112a78c7c362be61c7aa5dfa202bba
Scott Bailey <csb5t@virginia.edu>
2016-11-22 15:47:18 -0800

    Fix contrast check issue #33, due to scaling

--- histonets/histonets.py
+++ histonets/histonets.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def adjust_contrast(image, contrast):
     if (contrast < -100):
-        contrast = 0
+        contrast = -100
     elif (contrast > 100):
         contrast = 100
     contrast = (contrast + 100) / 100
