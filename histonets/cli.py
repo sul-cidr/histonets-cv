9fdaea67621fc6bfea8ba3e72af906dd46a3bac8
Scott Bailey <csb5t@virginia.edu>
2016-11-22 14:28:31 -0800

    Change language for clarity for user

--- histonets/cli.py
+++ histonets/cli.py
@@ -18,9 +18,16 @@ def download(image):
 
 @main.command()
 @io_handler
-@click.argument("contrast", type=click.IntRange(-100, 100))
-def contrast(image, contrast):
-    adjust_contrast(image, contrast)
+@click.argument("value", type=click.IntRange(-100, 100))
+def contrast(image, value):
+    adjust_contrast(image, value)
+
+
+@main.command()
+@io_handler
+@click.argument("value", type=click.IntRange(-100, 100))
+def contrast(image, value):
+    adjust_contrast(image, value)
 
 
 if __name__ == "__main__":
