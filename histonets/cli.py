fe830846c317494543322bcef2a5e1011226f79e
Scott Bailey <csb5t@virginia.edu>
2016-11-22 15:43:25 -0800

    Add command to adjust brightness

--- histonets/cli.py
+++ histonets/cli.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 import click
 
+<<<<<<< HEAD
 from .utils import io_handler
 from .histonets import adjust_contrast
+=======
+from .utils import open_image
+from .histonets import adjust_contrast, adjust_brightness
+>>>>>>> 601f060... Add command to adjust brightness
 
 
 @click.group()
@@ -26,8 +31,8 @@ def contrast(image, value):
 @main.command()
 @io_handler
 @click.argument("value", type=click.IntRange(-100, 100))
-def contrast(image, value):
-    adjust_contrast(image, value)
+def brightness(image, value):
+    adjust_brightness(image.image, value)
 
 
 if __name__ == "__main__":
