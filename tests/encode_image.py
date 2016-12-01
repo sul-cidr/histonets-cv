0cef315137c2945e5ae485eacb5b05339eb1444d
Scott Bailey <csb5t@virginia.edu>
2016-12-01 11:26:49 -0800

    Rewrite test to be OS independent

---
+++ tests/encode_image.py
@@ -0,0 +1,13 @@
+import base64
+
+import click
+
+
+@click.command()
+@click.option('--input', '-i', help='Input an image path')
+def main(input):
+    with open(input, 'rb') as image:
+        click.echo(base64.b64encode(image.read()).decode())
+
+if __name__ == '__main__':
+    main()
